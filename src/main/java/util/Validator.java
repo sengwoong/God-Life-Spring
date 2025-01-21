be6b285528dcb6cfc6ed395c23a58e230c7db1f5
sengwoong <rej287573@gmail.com>
2025-01-21 19:22:10 +0900

    :sparkles: 유저 벨리데이션 닉네임 검증

---
+++ src/main/java/util/Validator.java
@@ -0,0 +1,28 @@
+package com.example.GodLife.util;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.util.StringUtils;
+
+public class Validator {
+
+    public static void validateNickName(String nickName) {
+        if (nickName == null || nickName.length() < 3 || nickName.length() > 8) {
+            throw new IllegalArgumentException("닉네임은 3글자 이상 8글자 이하여야 합니다.");
+        }
+    }
+
+    public static void validateNotEmpty(String value, String message) {
+        if (!StringUtils.hasText(value)) {
+            throw new ConstraintViolationException(message,null);
+        }
+    }
+
+
+
+    public static void validateNotNegative(long value, String message) {
+        if (value <= 0 || value % 10 != 0) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+}
