1ed4b9db48b4b6f3ef363441e4e06e6e3980e6fc
sengwoong <rej287573@gmail.com>
2025-01-21 19:46:43 +0900

    :sparkles: 보카리포지터리 jpa 연결

---
+++ src/main/java/com/Dongo/GodLife/VocaBundle/Voca/Repository/VocaRepository.java
@@ -0,0 +1,11 @@
+package com.Dongo.GodLife.VocaBundle.Voca.Repository;
+
+import com.Dongo.GodLife.User.Model.User;
+import com.Dongo.GodLife.VocaBundle.Voca.Model.Voca;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VocaRepository extends JpaRepository<Voca, Long> {
+    Page<Voca> findByUser(User user, Pageable pageable);
+}
