3dcb2db296ca418ab8f0ab1751324dcfb967dc96
sengwoong <rej287573@gmail.com>
2025-01-24 20:40:49 +0900

    :sparkles: 스케줄 서비스 유저의 스케줄을 업데이트 합니다

---
+++ src/main/java/com/Dongo/GodLife/ScheduleBundle/Schedule/Exception/NotYourScheduleException.java
@@ -0,0 +1,7 @@
+package com.Dongo.GodLife.ScheduleBundle.Schedule.Exception;
+
+public class NotYourScheduleException extends Throwable {
+    public NotYourScheduleException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/Dongo/GodLife/ScheduleBundle/Schedule/Service/ScheduleService.java
+++ src/main/java/com/Dongo/GodLife/ScheduleBundle/Schedule/Service/ScheduleService.java
@@ -3,6 +3,7 @@ package com.Dongo.GodLife.ScheduleBundle.Schedule.Service;
 
 
 import com.Dongo.GodLife.ScheduleBundle.Schedule.Dto.ScheduleRequest;
+import com.Dongo.GodLife.ScheduleBundle.Schedule.Exception.NotYourScheduleException;
 import com.Dongo.GodLife.ScheduleBundle.Schedule.Model.Schedule;
 import com.Dongo.GodLife.User.Model.User;
 import jakarta.persistence.EntityNotFoundException;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import util.Validator;
 
 @Service
 @RequiredArgsConstructor
@@ -37,5 +39,24 @@ public class ScheduleService {
 
     public Page<Schedule> getAllschedulesByUserId(User user, Pageable pageable) {
         return scheduleRepository.findByUser(user, pageable);
+    }
+
+    public Schedule updateSchedule(long scheduleId, long userId , ScheduleRequest scheduleRequest) throws  NotYourScheduleException {
+
+        Schedule schedule = scheduleRepository.findById(scheduleId)
+                .orElseThrow(() -> new EntityNotFoundException("Schedule not found with ID: " + scheduleId));
+
+        Validator.validateNotEmpty(scheduleRequest.getScheduleTitle(), "Schedule title cannot be empty");
+        Validator.validateNotEmpty(scheduleRequest.getScheduleTitle(), "Schedule StartTime cannot be empty");
+        Validator.validateNotEmpty(scheduleRequest.getScheduleTitle(), "Schedule EndTime cannot be empty");
+
+        if (!schedule.getUser().getId().equals(userId)) {
+            throw new NotYourScheduleException("Access denied: User does not own the schedule");
+        }
+
+        schedule.setScheduleTitle(scheduleRequest.getScheduleTitle());
+        schedule.setStartTime(scheduleRequest.getStartTime());
+        schedule.setEndTime(scheduleRequest.getEndTime());
+        return scheduleRepository.save(schedule);
     }
 }
