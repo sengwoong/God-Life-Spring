92bcd83a6dfadc41427cd56d6842fd84a98e93e6
sengwoong <rej287573@gmail.com>
2025-01-24 20:32:15 +0900

    :sparkles: 스케줄 모델 생성

---
+++ src/main/java/com/Dongo/GodLife/ScheduleBundle/Schedule/Model/Schedule.java
@@ -0,0 +1,39 @@
+package com.Dongo.GodLife.ScheduleBundle.Schedule.Model;
+
+
+import com.Dongo.GodLife.User.Model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "schedule")
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @Column(nullable = false)
+    private String scheduleTitle;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
